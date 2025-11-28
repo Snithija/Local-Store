8ee034698f3eead11c9839813971abae2800184d
chiru5622 <chiranjeevi5622@gmail.com>
2025-11-28 12:45:18 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -66,7 +66,7 @@ const Header = () => {
   };
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 w-full border-b bg-card bg-white-300">
+    <header className="fixed top-0 left-0 right-0 z-50 w-full border-b bg-card bg-white-50">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
         {/* Logo */}
         <Link to="/" className="flex items-center gap-2">
